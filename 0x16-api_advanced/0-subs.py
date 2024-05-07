2a42fdf7202052e661a82df59d775e7a60a83cf1
Adnangad <gardobuyaadnan@gmail.com>
2024-05-07 10:46:35 -0700

    0-checker 3 error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,10 @@ Gets the number of subscribers of a subreddit.
 
 def number_of_subscribers(subreddit):
     """num of subs"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 {by /u/bdov_}'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    return data['data']['subscribers']
