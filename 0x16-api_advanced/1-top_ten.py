420480ec4acb3141654a5ef17665e721f22862a2
Adnangad <gardobuyaadnan@gmail.com>
2024-05-07 10:53:46 -0700

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""
+Gets the top ten titles of a subreddit.
+"""
 import requests
-"""
-Gets the number of subscribers of a subreddit.
-"""
 
 
 def top_ten(subreddit):
     """num of subs"""
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {'User-Agent': 'Custom User Agent'}
+    headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     if response.status_code == 200:
