e71e47087c13b5b92326f905078b6851c6abb992
Adnangad <gardobuyaadnan@gmail.com>
2024-05-07 07:47:15 -0700

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,11 +3,12 @@ import requests
 """Gets the number of subscribers of a subreddit."""
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """num of subs"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {'limit': 10}
+    response = requests.get(url, params=params, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
