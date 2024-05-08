e884ba7d85eb0ee040a21a3dc85d1e1a19b607b2
Adnangad <gardobuyaadnan@gmail.com>
2024-05-08 12:54:09 +0000

    web_debugging-3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # debugging
 exec {'fix-wordpress':
-  command  =>  'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path     =>  'usr/local/bin/:/bin/'
+  command =>  'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    =>  'usr/local/bin/:/bin/'
 }
